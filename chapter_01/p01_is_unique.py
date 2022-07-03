83201cc184bb6201e737458123176f47ea77745e
Hala Elwazery <54862182+helwazery@users.noreply.github.com>
2022-07-03 22:49:18 +0200

    Update p01_is_unique.py

--- chapter_01/p01_is_unique.py
+++ chapter_01/p01_is_unique.py
@@ -1,3 +1,6 @@
+#Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you
+#cannot use additional data structures?
+------------------------------------------------------------------------------------
 import time
 import unittest
 from collections import defaultdict
