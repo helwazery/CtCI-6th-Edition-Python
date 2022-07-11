7d84ddcc0a531b402890c498a81518d0398f4219
Hala Elwazery <54862182+helwazery@users.noreply.github.com>
2022-07-11 23:36:59 +0200

    Update p06_successor.py

--- chapter_04/p06_successor.py
+++ chapter_04/p06_successor.py
@@ -1,3 +1,8 @@
+"""
+Successor: Write an algorithm to find the "next" node (i.e., in-order successor) of a given node in a
+binary search tree. You may assume that each node has a link to its parent.
+"""
+
 from chapter_04.binary_search_tree import BinarySearchTree
 
 
