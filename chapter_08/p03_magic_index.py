7b729377b3edd2f211d961b5471728c5e57cb36e
Hala Elwazery <54862182+helwazery@users.noreply.github.com>
2022-07-12 22:52:05 +0200

    Update p03_magic_index.py

--- chapter_08/p03_magic_index.py
+++ chapter_08/p03_magic_index.py
@@ -1,3 +1,14 @@
+""""
+Magic Index: A magic index in an array A [e ... n -1] is defined to be an index such that A[ i] =
+i. Given a sorted array of distinct integers, write a method to find a magic index, if one exists, in
+array A.
+FOLLOW UP
+What if the values are not distinct?
+Hints: # 170, #204, #240, #286, #340
+
+https://medium.com/@y.kononchuk/8-3-magic-index-872c44637c60
+
+"""""
 NOT_FOUND = -1
 
 
