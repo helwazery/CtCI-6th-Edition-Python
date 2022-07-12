69fa46521786f8bae021702dc777ed35247062ab
Hala Elwazery <54862182+helwazery@users.noreply.github.com>
2022-07-12 23:02:28 +0200

    Update p05_recursive_multiply.py

--- chapter_08/p05_recursive_multiply.py
+++ chapter_08/p05_recursive_multiply.py
@@ -1,3 +1,11 @@
+""""
+Recursive Multiply: Write a recursive function to multiply two positive integers without using the
+* operator. You can use addition, subtraction, and bit shifting, but you should minimize the number
+of those operations.
+https://www.youtube.com/watch?v=VapDVrsERsA
+
+""""
+
 # naive implementation
 def multiply(a, b, answer):
     if answer == 0 and a != 0 and b != 0:
