974f31a3f9f042e72f95b82035d4de96ecb6e765
Hala Elwazery <54862182+helwazery@users.noreply.github.com>
2022-07-18 00:51:26 +0200

    Update p10_paint_fill.py

--- chapter_08/p10_paint_fill.py
+++ chapter_08/p10_paint_fill.py
@@ -1,3 +1,11 @@
+"""
+Paint Fill: Implement the "paint nil" function that one might see on many image editing programs.
+That is, given a screen (represented by a two-dimensional array of colors), a point, and a new color,
+nil in the surrounding area until the color changes from the original color.
+
+SOLUTION: https://www.youtube.com/watch?v=VuiXOc81UDM
+
+"""
 import unittest
 from typing import List
 
