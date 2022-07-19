d2d3c06608b724d79dc38b6686a66cd47043213d
Hala Elwazery <54862182+helwazery@users.noreply.github.com>
2022-07-19 22:27:50 +0200

    Update p11_coins.py

--- chapter_08/p11_coins.py
+++ chapter_08/p11_coins.py
@@ -1,3 +1,9 @@
+"""
+Given an innnite number of quarters (25 cents), dimes (10 cents), nickels (5 cents), and
+pennies (1 cent), write code to calculate the number of ways of representing n cents.
+SOLUTION: https://www.youtube.com/watch?v=H9bfqozjoqs
+"""
+
 STANDARD_COIN_SIZES = [1, 5, 10, 25]
 
 
